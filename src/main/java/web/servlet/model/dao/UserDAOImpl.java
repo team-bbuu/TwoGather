fbaafcd05387d5334b409e268d3a4f5454a8fbac
sunmin_hwang <tommy08190@naver.com>
2024-09-14 15:33:31 +0900

    feat 회원가입 기능 추가

--- src/main/java/web/servlet/model/dao/UserDAOImpl.java
+++ src/main/java/web/servlet/model/dao/UserDAOImpl.java
@@ -21,7 +21,7 @@ public class UserDAOImpl implements UserDAO {
 		}catch (NamingException e) {
 			System.out.println(e);
 		}
-		
+			
 		/*
 		try {
 			Class.forName(ServerInfo.DRIVER_NAME);
@@ -51,24 +51,108 @@ public class UserDAOImpl implements UserDAO {
 	}
 	
 	@Override
-	public Boolean checkID(String id) {
+	public User FindUser(String id) throws SQLException {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		User user= null;
 		
-		return null;
+		try {
+			conn = getConnection();
+			String query = "SELECT id, parner_id, img_src, password, nickname, mobile, birthdate, email, gender, address, maching, start_date, break_date FROM user WHERE id=?";
+			ps = conn.prepareStatement(query);
+			ps.setString(1, id);
+			
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				user = new User(id, rs.getString("parner_id"), rs.getString("img_src"), rs.getString("password"), rs.getString("nickname"), rs.getString("mobile"), 
+						rs.getString("birthdate"), rs.getString("email"), rs.getString("gender"), rs.getString("address"), rs.getString("maching"), rs.getString("start_date"),
+						rs.getString("break_date"));
+			}
+		} finally {
+			closeAll(rs, ps, conn);
+		}
+		return user;
 	}
 	@Override
-	public Boolean checkEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean checkEmail(String email) throws SQLException {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		boolean isExist= false;
+		
+		try {
+			conn = getConnection();
+			String query = "SELECT id FROM user WHERE email=?";
+			ps = conn.prepareStatement(query);
+			ps.setString(1, email);
+			
+			rs = ps.executeQuery();
+			isExist = rs.next();
+		} finally {
+			closeAll(rs, ps, conn);
+		}
+		return isExist;
 	}
 	@Override
-	public Boolean checkMobile(String mobile) {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean checkMobile(String mobile) throws SQLException {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		boolean isExist= false;
+		
+		try {
+			conn = getConnection();
+			String query = "SELECT id FROM user WHERE mobile=?";
+			ps = conn.prepareStatement(query);
+			ps.setString(1, mobile);
+			
+			rs = ps.executeQuery();
+			isExist = rs.next();
+		} finally {
+			closeAll(rs, ps, conn);
+		}
+		return isExist;
 	}
 	@Override
-	public User register(User vo) {
-		// TODO Auto-generated method stub
-		return null;
+	public void register(User vo) throws SQLException {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		
+		try {
+			conn = getConnection();
+			String query = "INSERT INTO user(id, img_src, password, nickname, mobile, birthdate, email, gender, address) VALUES(?,?,?,?,?,?,?,?,?)";
+			ps = conn.prepareStatement(query);
+			
+			ps.setString(1, vo.getId());
+			ps.setString(2, "image/image.jpg");
+			ps.setString(3, vo.getPassword());
+			ps.setString(4, vo.getNickname());
+			ps.setString(5, vo.getMobile());
+			ps.setString(6, vo.getBirthdate());
+			ps.setString(7, vo.getEmail());
+			ps.setString(8, vo.getGender());
+			ps.setString(9, vo.getAddress());
+			
+			ps.executeUpdate();
+			
+			
+		} finally {
+			closeAll(ps, conn);
+		}
+		
+		try {
+			conn = getConnection();
+			String query = "INSERT INTO category(categoty_name, user_id) VALUES('식비,패션',?)";
+			ps = conn.prepareStatement(query);
+			
+			ps.setString(1, vo.getId());
+			
+			System.out.println(ps.executeUpdate() + "명 등록");
+			
+		} finally {
+			closeAll(ps, conn);
+		}
 	}
 	@Override
 	public User login(String id, String password) {
@@ -91,17 +175,7 @@ public class UserDAOImpl implements UserDAO {
 		
 	}
 	@Override
-	public void breakPartner(String id) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
 	public void deleteUser(String id) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void invitePartner(String id, String partnerId) {
 		// TODO Auto-generated method stub
 		
 	}
